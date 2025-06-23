0c41c911897f669f7bc7ee9b4155830e9f4bd388
sonidori98 <G-Free_159@proton.me>
2025-06-24 00:06:28 +0900

    🎉 First commit
    First commit

--- init.lua
+++ init.lua
@@ -2,6 +2,8 @@
 -- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
 local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
+vim.opt.guicursor = "i:block"
+
 if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
   -- stylua: ignore
   local result = vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
